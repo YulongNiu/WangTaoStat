97567b2d5e4d72009d4fd4dc3e7bcb4ae33504a3
YulongNiu <niuylscu@gmail.com>
2017-02-21 14:36:48 +0800

    add process data

--- PDHD_pair.R
+++ PDHD_pair.R
@@ -1,2 +1,23 @@
-##################### deal with data ###################
+setwd('/home/Yulong/RESEARCH/WangTao/Rawdata/')
 
+##################### deal with data ###################
+PD <- read.csv('PD.csv')
+PD <- PD[, -2]
+PD[, 1] <- paste0('Name', 1:nrow(PD))
+colnames(PD) <- c('PatientName', 'Time', 'HydroxyVitaminD',
+                  'BlootProtein', 'AlkalinePhosphatase', 'BloodUreaNitrogen',
+                  'Creatinine', 'BloodUricAcid', 'Calcium', 'Phosphorum',
+                  'iPHT', 'CReactiveProtein', 'BNP')
+
+HD <- read.csv('HD.csv')
+HD <- HD[, -2]
+HD[, 1] <- paste0('Name', 1:nrow(HD))
+colnames(HD) <- c('PatientName', 'Time', 'HydroxyVitaminD',
+                  'BlootProtein', 'AlkalinePhosphatase', 'BloodUreaNitrogen',
+                  'Creatinine', 'BloodUricAcid', 'Calcium', 'Phosphorum',
+                  'iPHT', 'CReactiveProtein', 'BNP')
+
+save(PD, HD, file = 'PDHDprocess.RData')
+########################################################
+
+#######################
